66adf219c2d3dd7c83f2f52cf2be059acf1ccd55
Jack Rosenthal <jack@rosenth.al>
2018-12-22 00:47:45 -0700

    Change to a setup script not dependant on README

--- setup.py
+++ setup.py
@@ -4,42 +4,15 @@
 # pip runs it for you when you type commands.
 
 from setuptools import setup
-import os
-import codecs
-import re
-
-here = os.path.abspath(os.path.dirname(__file__))
-
-
-def read(*parts):
-    with codecs.open(os.path.join(here, *parts), 'r') as fp:
-        return fp.read()
-
-
-with codecs.open(os.path.join(here, 'README.rst'), encoding='utf-8') as f:
-    long_description = f.read()
-
-deliv_m = re.search(
-    r":Deliverable:\s*(\d+)",
-    long_description,
-    re.DOTALL)
-
-if not deliv_m:
-    raise ValueError("Could not find what deliverable you are working on")
-deliv = int(deliv_m.group(1))
 
 setup(
     name='slytherlisp',
-    version='1.1.{}-devel'.format(deliv),
-
+    version='1.2.0-devel',
     description='Scheme like programming language for CSCI-400 at Mines',
-    long_description=long_description,
-
+    long_description='',
     url='https://lambda.mines.edu',
-
     author='Jack Rosenthal',
     author_email='jrosenth@Mines.EDU',
-
     license='Closed source; do not share with other students '
             'in (or who will be in) this course.',
 
